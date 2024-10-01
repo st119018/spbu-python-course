952467541da309f99c77fdaa0499ec211274adec
elina <elina@Ubuntu.myguest.virtualbox.org>
2024-10-01 17:57:51 +0300

    Change matrix representation; change type checking of matrix elements

--- project/matrix.py
+++ project/matrix.py
@@ -3,6 +3,10 @@
 Classes
 -------
 Matrix
+
+Functions
+---------
+is_matrix(args)
 """
 
 
@@ -12,16 +16,10 @@ class Matrix:
     Attributes
     ----------
     elements : list
-        List of elements of matrix
-    rows : int
-        Number of rows in matrix
-    columns : int
-        Number of columns in matrix
+        List of lists; elements of matrix
 
     Methods
     -------
-    __is_num(args)
-        Check if matrix consists of numbers
     __add__(matrix)
         Operator for matrices adding
     __mul__(matrix)
@@ -30,56 +28,28 @@ class Matrix:
         Transpose matrix
     """
 
-    def __init__(self, rows: int, columns: int, args: list):
+    def __init__(self, args: list):
         """Set attributes for object
 
         Parameters
         ----------
-        rows : int
-            Number of rows in matrix
-        columns : int
-            Number of columns in matrix
-        elements : list
-            List of elements of matrix
+        args : list
+            List of lists representing elements of matrix.
+            Example of input: [[1, 2], [3, 4]].
 
         Raises
         ------
         ValueError
-            If rows or columns aren't positive
-        IndexError
-            If size of elements is less than size of matrix or zero
-        """
-        if rows < 1 or columns < 1:
-            raise ValueError("Rows and columns of matrix have to be positive")
-        if len(args) == 0:
-            raise IndexError("Matrix has no elements")
-        if rows * columns != len(args):
-            raise IndexError(
-                f"Expected {rows * columns} arguments" f" but recieved: {len(args)}"
-            )
-        self.__is_num(args)
-        self.columns = columns
-        self.rows = rows
-        self.elements = args
-
-    def __is_num(self, args):
-        """Check if given list consists of numbers
-
-        Parameters
-        ----------
-        args : list
-
-        Raises
-        ------
+            If input is empty
         TypeError
-            If not numbers are in matrix
+            If type of input is not list of lists
+        ValueError
+            If lists in input are not same length
+        TypeError
+            If non-numbers are in matrix
         """
-        try:
-            sum = 0
-            for i in range(len(args)):
-                sum += args[i]
-        except TypeError:
-            print("Type error: matrix doesn't consist of numbers")
+        is_matrix(args)
+        self.elements = args
 
     def __add__(self, matrix):
         """Add matricies and return new matrix
@@ -97,12 +67,17 @@ class Matrix:
         """
         if type(matrix) != Matrix:
             raise TypeError(f"Incorrect type: {type(matrix)}, " "expected: Matrix.")
-        if self.rows != matrix.rows and self.columns != matrix.columns:
+        rows = len(self.elements)
+        col = len(self.elements[0])
+        if rows != len(matrix.elements) or col != len(matrix.elements[0]):
             raise IndexError("Different size of matrices")
-        new_elements = [0] * len(self.elements)
-        for i in range(len(self.elements)):
-            new_elements[i] += self.elements[i] + matrix.elements[i]
-        return Matrix(self.rows, self.columns, new_elements)
+        new_elements = []
+        for i in range(rows):
+            new_row = [0] * col
+            for j in range(col):
+                new_row[j] += self.elements[i][j] + matrix.elements[i][j]
+            new_elements.append(new_row)
+        return Matrix(new_elements)
 
     def __mul__(self, matrix):
         """Multiply matrices and return new matrix
@@ -120,22 +95,60 @@ class Matrix:
         """
         if type(matrix) != Matrix:
             raise TypeError(f"Incorrect type: {type(matrix)}, " "expected: Matrix.")
-        if self.columns != matrix.rows:
+        rows = len(self.elements)
+        col = len(self.elements[0])
+        if col != len(matrix.elements):
             raise IndexError("Matrices can't be multiplied")
-        new_elements = [0] * (self.rows * matrix.columns)
-        for i in range(self.rows):
-            for j in range(matrix.columns):
-                for k in range(self.columns):
-                    new_elements[i * matrix.columns + j] += (
-                        self.elements[i * self.columns + k]
-                        * matrix.elements[k * matrix.columns + j]
-                    )
-        return Matrix(self.rows, matrix.columns, new_elements)
+        new_elements = []
+        for i in range(rows):
+            new_row = [0] * len(matrix.elements[0])
+            for j in range(len(matrix.elements[0])):
+                for k in range(col):
+                    new_row[j] += self.elements[i][k] * matrix.elements[k][j]
+            new_elements.append(new_row)
+        return Matrix(new_elements)
 
     def transpos(self):
         """Return transposed matrix"""
-        el = [0] * (self.rows * self.columns)
-        for i in range(self.rows):
-            for j in range(self.columns):
-                el[j * self.rows + i] = self.elements[i * self.columns + j]
-        return Matrix(self.columns, self.rows, el)
+        new_el = []
+        for j in range(len(self.elements[0])):
+            new_row = [0] * len(self.elements)
+            for i in range(len(self.elements)):
+                new_row[i] += self.elements[i][j]
+            new_el.append(new_row)
+        return Matrix(new_el)
+
+
+def is_matrix(args: list):
+    """Check if input has form of matrix.
+
+    Parameters
+    ----------
+    args : list
+        List of lists representing elements of matrix.
+        Example of input: [[1, 2], [3, 4]].
+
+    Raises
+    ------
+    ValueError
+        If input list is empty
+    TypeError
+        If type of input is not list of lists
+    ValueError
+        If empty list in input
+    ValueError
+        If lists in input are not same length
+    TypeError
+        If non-numbers are in matrix
+    """
+    if len(args) == 0:
+        raise ValueError("Matrix is empty")
+    if not (all(isinstance(row, list) for row in args)):
+        raise TypeError("Matrix should be list of lists")
+    if not (all(len(row) != 0 for row in args)):
+        raise ValueError("Empty row in matrix")
+    if not (all(len(args[0]) == len(l) for l in args)):
+        raise ValueError("Matrix has rows with different length")
+    for row in args:
+        if not (all(isinstance(el, (int, float)) for el in row)):
+            raise TypeError("Matrix doesn't consist of numbers")
