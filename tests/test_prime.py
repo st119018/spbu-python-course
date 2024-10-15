3a99ac08eb07e6740c03980d1dc3bc93cc58b9a7
elina <elina@Ubuntu.myguest.virtualbox.org>
2024-10-15 22:32:50 +0300

    Change tests

--- tests/test_prime.py
+++ tests/test_prime.py
@@ -2,27 +2,43 @@ import pytest
 from project.prime import get_prime, prime_generator
 
 
-@pytest.mark.parametrize("k, prime", [(1, 2), (5, 11), (6, 13)])
-def test_prime_get_prime(k, prime):
+@pytest.mark.parametrize(
+    "k, prime", [(1, 2), (2, 3), (3, 5), (4, 7), (5, 11), (6, 13), (7, 17)]
+)
+def test_prime_evenly_increasing_sequence(k, prime):
     @get_prime
-    def func(a, b):
-        pass
+    def gen():
+        return prime_generator()
 
-    assert func(k) == prime
+    assert gen(k) == prime
 
 
-def test_prime_get_prime_negative_index():
+@pytest.mark.parametrize(
+    "k, prime", [(1, 2), (2, 3), (5, 11), (20, 71), (21, 73), (27, 103)]
+)
+def test_prime_unevenly_increasing_sequence(k, prime):
     @get_prime
-    def func(a, b):
-        pass
+    def gen():
+        return prime_generator()
+
+    assert gen(k) == prime
+
+
+def test_prime_invalid_index():
+    @get_prime
+    def gen():
+        return prime_generator()
 
     with pytest.raises(ValueError):
-        func(-1)
+        gen(-1)
+
+    assert gen(4) == 7
+    with pytest.raises(ValueError):
+        gen(3)  # got 4-th prime number before
 
 
-@pytest.mark.parametrize("k, prime", [(1, 2), (2, 3), (20, 71)])
-def test_prime_generator(k, prime):
+def test_prime_generator():
     gen = prime_generator()
-    for _ in range(k - 1):
-        next(gen)
-    assert next(gen) == prime
+    gen_primes = [next(gen) for _ in range(7)]
+    primes = [2, 3, 5, 7, 11, 13, 17]
+    assert gen_primes == primes
