445907a064941d77aa3b87544039c091d9e52465
elina <elina@Ubuntu.myguest.virtualbox.org>
2024-10-17 16:49:52 +0300

    Fix test

--- tests/test_prime.py
+++ tests/test_prime.py
@@ -1,27 +1,16 @@
 import pytest
 from project.prime import get_prime, prime_generator
 
-
-@pytest.mark.parametrize(
-    "k, prime", [(1, 2), (2, 3), (3, 5), (4, 7), (5, 11), (6, 13), (7, 17)]
-)
-def test_prime_evenly_increasing_sequence(k, prime):
+# without parametrize
+def test_prime_evenly_increasing_sequence():
     @get_prime
     def gen():
         return prime_generator()
 
-    assert gen(k) == prime
+    generated_primes = [gen(k) for k in range(1, 10)]
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23]
 
-
-@pytest.mark.parametrize(
-    "k, prime", [(1, 2), (2, 3), (2, 3), (20, 71), (20, 71), (27, 103)]
-)
-def test_prime_unevenly_increasing_sequence(k, prime):
-    @get_prime
-    def gen():
-        return prime_generator()
-
-    assert gen(k) == prime
+    assert generated_primes == primes
 
 
 def test_prime_invalid_index():
@@ -42,3 +31,17 @@ def test_prime_generator():
     gen_primes = [next(gen) for _ in range(7)]
     primes = [2, 3, 5, 7, 11, 13, 17]
     assert gen_primes == primes
+
+
+@get_prime
+def gen():
+    return prime_generator()
+
+
+# using parametrize
+@pytest.mark.parametrize(
+    "k, prime", [(1, 2), (2, 3), (2, 3), (20, 71), (20, 71), (27, 103)]
+)
+def test_prime_unevenly_increasing_sequence(k, prime):
+
+    assert gen(k) == prime
